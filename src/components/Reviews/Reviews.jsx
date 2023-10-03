7d7d241c719fe49515940558658dcbcab2647239
Breno Dobroski <brenodobroski@gmail.com>
2023-10-03 20:15:47 -0300

    feat: add reviews section

---
+++ src/components/Reviews/Reviews.jsx
@@ -0,0 +1,24 @@
+import "./Reviews.scss";
+const Reviews = () => {
+  return (
+    <div className="Reviews">
+      <div className="Reviews__text">
+        <h2 className="Reviews__h2">
+          This is my favourite <br />
+          Pet care agency
+        </h2>
+        <p className="Reviews__p">
+          The only place i will leave my little girl when <br /> I go away. The
+          only issue is she dont want <br /> to come home because she is having
+          so <br /> much fun. The best part is the accessibillity <br /> to the
+          staff and pictures on Facebook of <br /> playtime. I could not feel
+          more confident in <br /> who is taking care of my pup while being
+          <br /> out of town.
+        </p>
+      </div>
+      <img src="Reviews.svg" alt="" />
+    </div>
+  );
+};
+
+export default Reviews;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Hero from "../components/Hero/Hero";
 import HowToTake from "../components/HowToTake/HowToTake";
 import Navbar from "../components/Navbar/Navbar";
 import OurServices from "../components/OurServices/OurServices";
+import Reviews from "../components/Reviews/Reviews";
 import SafeHands from "../components/SafeHands/SafeHands";
 import SectionHelp from "../components/SectionHelp/SectionHelp";
 import Stats from "../components/Stats/Stats";
@@ -17,6 +18,7 @@ const Home = () => {
       <OurServices />
       <Stats />
       <SafeHands />
+      <Reviews />
 
       <div>Home</div>
     </div>
